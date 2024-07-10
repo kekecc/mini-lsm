5b6e8a2d01c297fcbd48ed7bd580514128cf92ef
kk <1606295957@qq.com>
2024-07-10 19:41:20 +0800

    feat: finish DAY 3 Task 1 BlockBuilder struc t

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,6 +1,8 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use bytes::BufMut;
+
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
@@ -20,22 +22,51 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        if self.data.len() + self.offsets.len() + key.len() + value.len() + 2 > self.block_size
+            && !self.is_empty()
+        {
+            false;
+        }
+
+        let key_len = key.len() as u16;
+        let value_len = value.len() as u16;
+
+        // key-value 的索引
+        self.offsets.push(self.data.len() as u16);
+
+        self.data.put_u16(key_len);
+        self.data.extend_from_slice(key.raw_ref());
+        self.data.put_u16(value_len);
+        self.data.extend_from_slice(value);
+
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty() {
+            panic!("BlockBuilder is empty when called build");
+        }
+
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
